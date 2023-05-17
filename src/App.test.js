1b3d71ac5e1c0fe840c7d62d55fbead892cf45f9
alepatro <aalepatro@gmail.com>
2023-05-18 00:29:49 +0300

    tech-9

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 // import { render, screen } from '@testing-library/react';
-import ReactDOM from 'react-dom';
-import MainApp from './App';
+// import ReactDOM from 'react-dom';
+// import MainApp from './App';
 
-test('renders learn react link', () => {
-  const container = document.createElement("div");
-  ReactDOM.render(<MainApp />, container)
-  ReactDOM.unmountComponentAtNode(container);
-});
+// test('renders learn react link', () => {
+//   const container = document.createElement("div");
+//   ReactDOM.render(<MainApp />, container)
+//   ReactDOM.unmountComponentAtNode(container);
+// });
--- src/api/constants.js
+++ src/api/constants.js
@@ -2,4 +2,4 @@
  * @const
  * @type {string} server base url
  */
-export const SERVER_BASE_URL = 'https://eoeo/';
+export const SERVER_BASE_URL = 'http://eoeo/';
